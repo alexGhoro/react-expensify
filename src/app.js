e6d3e63e6e621850238f1a54943a66c486aac02f
Alejandro Matute <alexghoro@outlook.com>
2018-03-07 11:44:17 -0400

    Delete unnecesary lines

--- src/app.js
+++ src/app.js
@@ -12,16 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-
-
-store.dispatch(addExpense({ description: 'water bill', amount: 10, createdAt: 10 }));
-store.dispatch(addExpense({ description: 'gas bill', amount: 300, createdAt: 11 }));
-store.dispatch(addExpense({ description: 'Rent', amount:40, createdAt: 12 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter/>
