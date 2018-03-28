7178c47dda4b7f551085ebabc55a6796df0a08db
Alejandro Matute <alexghoro@outlook.com>
2018-03-28 14:04:16 -0400

    Fix typo

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if(process.env.NODE_ENV === 'test'){
-  requiere.('dotenv').config({ path: '.env.test'});
+  requiere('dotenv').config({ path: '.env.test'});
 }else if (process.env.NODE_ENV === 'development'){
-  requiere.('dotenv').config({ path: '.env.development'});
+  requiere('dotenv').config({ path: '.env.development'});
 }
 
 module.exports = (env) => {
